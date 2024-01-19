e4b9b7c0ee9a7be771cc925f041d70af7c5d2643
Markayel Orelus <142757026+Morelus@users.noreply.github.com>
2024-01-18 16:30:00 -0800

    added updates

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -3,9 +3,7 @@ import React, { useState } from "react";
 const AboutUs = () => {
   return (
     <div className="aboutUsContainer">
-      
       <h4>
-        
         "Meet our dynamic team of four students who embarked on an exciting
         journey through a rigorous coding bootcamp. Comprising Derek, Jacob,
         Markayel, and Corey, our team is fueled by a relentless passion for
@@ -16,21 +14,20 @@ const AboutUs = () => {
         we're not just students; we're a tight-knit community of aspiring
         developers eager to make a mark in the ever-evolving tech landscape."
       </h4>
-      <h5> 
-      Derek:
-      As a dedicated student, I specialize in both front-end and back-end web development, with a focus on React.js and Ruby on Rails. My knowledge lies in creating dynamic and responsive user interfaces using React.js and building robust web applications on the server-side with Ruby on Rails. knowledge  in crafting efficient database models and RESTful APIs, I am passionate about delivering high-quality software solutions. My comprehensive skill set allows me to contribute effectively to the end-to-end development process.</h5>
-    <h5>
-    Jacob: 
-    Hey, I'm Jacob Amezola, a passionate software developer skilled in Ruby on Rails, React.js, and Javascript. With a knack for creative problem-solving, I bring a unique blend of innovation to my work. Eager to learn and contribute to cutting-edge projects, let's code the future together!
-    </h5>
       <h5>
-      Markayel:
-      Hi, I'm Markayel! Having successfully delivered high-quality solutions in collaborative team settings, I possess a solid foundation in data structures, algorithms, and distributed systems. I bring a creative approach to cutting-edge projects and collaborating with a diverse team. My passion for software development, coupled with a commitment to excellence has prepared me for unique and fun collaborations. 
+        Derek:
+        As a dedicated student, I specialize in both front-end and back-end web development, with a focus on React.js and Ruby on Rails. My knowledge lies in creating dynamic and responsive user interfaces using React.js and building robust web applications on the server-side with Ruby on Rails. knowledge  in crafting efficient database models and RESTful APIs, I am passionate about delivering high-quality software solutions. My comprehensive skill set allows me to contribute effectively to the end-to-end development process.</h5>
+      <h5>
+        Jacob:
+        Hey, I'm Jacob Amezola, a passionate software developer skilled in Ruby on Rails, React.js, and Javascript. With a knack for creative problem-solving, I bring a unique blend of innovation to my work. Eager to learn and contribute to cutting-edge projects, let's code the future together!
       </h5>
       <h5>
-     
-      Corey:
-      Hi, I'm Corey Cass, a passionate and dedicated software developer with expertise in Ruby on Rails, React.js, and JavaScript. With a background in technical support and customer service, I bring a unique blend of problem-solving skills and a strong attention to detail to my work. I thrive in collaborative environments and am constantly seeking opportunities to expand my knowledge and contribute to innovative projects.
+        Markayel:
+        Hi, I'm Markayel! Having successfully delivered high-quality solutions in collaborative team settings, I possess a solid foundation in data structures, algorithms, and distributed systems. I bring a creative approach to cutting-edge projects and collaborating with a diverse team. My passion for software development, coupled with a commitment to excellence has prepared me for unique and fun collaborations.
+      </h5>
+      <h5>
+        Corey:
+        Hi, I'm Corey Cass, a passionate and dedicated software developer with expertise in Ruby on Rails, React.js, and JavaScript. With a background in technical support and customer service, I bring a unique blend of problem-solving skills and a strong attention to detail to my work. I thrive in collaborative environments and am constantly seeking opportunities to expand my knowledge and contribute to innovative projects.
       </h5>
     </div>
   );
